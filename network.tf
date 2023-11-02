4c8615be0c396e8415882f0e0fca1b6b28a6592c
Sven Schliesing <s.schliesing@tagesschau.de>
2023-11-02 14:41:25 +0100

    use viewer

--- network.tf
+++ network.tf
@@ -16,3 +16,10 @@ resource "google_compute_subnetwork" "test2" {
   ip_cidr_range = "10.1.0.0/24"
   network       = google_compute_network.test.id
 }
+
+resource "google_compute_subnetwork" "test3" {
+  name          = "test3"
+  region        = "europe-west3"
+  ip_cidr_range = "10.2.0.0/24"
+  network       = google_compute_network.test.id
+}
