d205589d38e15048c73f466adca4cb57a59819c6
Sven Schliesing <s.schliesing@tagesschau.de>
2023-11-03 14:58:20 +0100

    deploy always

--- network.tf
+++ network.tf
@@ -9,3 +9,10 @@ resource "google_compute_subnetwork" "test" {
   ip_cidr_range = "10.0.0.0/24"
   network       = google_compute_network.test.id
 }
+
+resource "google_compute_subnetwork" "test2" {
+  name          = "test2"
+  region        = "europe-west3"
+  ip_cidr_range = "10.1.0.0/24"
+  network       = google_compute_network.test.id
+}
